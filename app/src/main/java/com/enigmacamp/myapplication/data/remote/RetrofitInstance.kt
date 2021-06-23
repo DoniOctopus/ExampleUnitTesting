64d81b5e5abeb22c4d014e7be519f0dec29d7fa8
edwardsuwirya <edward.suwirya@ffi.co.id>
2021-06-23 07:01:08 +0700

    Cleanup some code

--- app/src/main/java/com/enigmacamp/myapplication/data/remote/RetrofitInstance.kt
+++ app/src/main/java/com/enigmacamp/myapplication/data/remote/RetrofitInstance.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitInstance {
-    val BASE_URL = "https://pixabay.com"
+    private const val BASE_URL = "https://pixabay.com"
 
     private val retrofit =
         Retrofit.Builder()
--- app/src/main/java/com/enigmacamp/myapplication/data/repository/PixabayRepository.kt
+++ app/src/main/java/com/enigmacamp/myapplication/data/repository/PixabayRepository.kt
@@ -3,13 +3,13 @@ package com.enigmacamp.myapplication.data.repository
 import com.enigmacamp.myapplication.data.remote.PixabayAPI
 import com.enigmacamp.myapplication.data.remote.responses.ImageResponse
 
-class PixabayRepository(val pixabayAPI: PixabayAPI) {
+class PixabayRepository(private val pixabayAPI: PixabayAPI) {
     suspend fun searchImage(name: String): ImageResponse? {
         val response = pixabayAPI.searchForImage(name)
-        if (response.isSuccessful) {
-            return response.body()
+        return if (response.isSuccessful) {
+            response.body()
         } else {
-            return null
+            null
         }
     }
 }
